2f8cb3833500dca159d0e1c2f24bfa94c3cf8bd7
Amit Kumar Mondal <admin@amitinside.com>
2016-03-08 18:44:49 +0100

    Added Semantic Values for Latitude and Longitude

--- com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
+++ com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
@@ -9,8 +9,11 @@ public final class MockDataProvider {
 	private static Random random = new Random();
 
 	public static DataPayload mock() {
+		final double longitude = Math.random() * Math.PI * 2;
+		final double latitude = Math.acos((Math.random() * 2) - 1);
+
 		return new DataPayload.Builder().co(random.nextDouble()).no2(random.nextDouble()).o3(random.nextDouble())
-				.so2(random.nextDouble()).latitude(random.nextDouble()).longitude(random.nextDouble()).build();
+				.so2(random.nextDouble()).latitude(latitude).longitude(longitude).build();
 	}
 
 	private MockDataProvider() {
