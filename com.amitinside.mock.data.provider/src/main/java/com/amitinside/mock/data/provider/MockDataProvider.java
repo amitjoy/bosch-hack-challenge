876649f7c03257fd90bc73c449aee59a93b7faa8
Amit Kumar Mondal <admin@amitinside.com>
2016-03-08 20:04:41 +0100

    Added new method that returns random inclusive double values

--- com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
+++ com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
@@ -1,19 +1,24 @@
 package com.amitinside.mock.data.provider;
 
-import java.util.Random;
-
 import com.amitinside.core.api.DataPayload;
 
 public final class MockDataProvider {
 
-	private static Random random = new Random();
+	private static double doubleRandomInclusive(final double max, final double min) {
+		final double r = Math.random();
+		if (r < 0.5) {
+			return (((1 - Math.random()) * (max - min)) + min);
+		}
+		return ((Math.random() * (max - min)) + min);
+	}
 
 	public static DataPayload mock() {
 		final double longitude = Math.random() * Math.PI * 2;
 		final double latitude = Math.acos((Math.random() * 2) - 1);
 
-		return new DataPayload.Builder().co(random.nextDouble()).no2(random.nextDouble()).o3(random.nextDouble())
-				.so2(random.nextDouble()).latitude(latitude).longitude(longitude).build();
+		return new DataPayload.Builder().co(doubleRandomInclusive(0.0d, 2.0d)).no2(doubleRandomInclusive(1.0d, 4.0d))
+				.o3(doubleRandomInclusive(1.0d, 3.0d)).so2(doubleRandomInclusive(0.0d, 1.0d)).latitude(latitude)
+				.longitude(longitude).build();
 	}
 
 	private MockDataProvider() {
