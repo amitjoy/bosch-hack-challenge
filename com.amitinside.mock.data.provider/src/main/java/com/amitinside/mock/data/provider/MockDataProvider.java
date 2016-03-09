d7443b0e07bca9ec5af0bcb170813e39bb5f8e99
Amit Kumar Mondal <admin@amitinside.com>
2016-03-09 08:58:23 +0100

    Modified Utility Methods

--- com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
+++ com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
@@ -1,23 +1,23 @@
 package com.amitinside.mock.data.provider;
 
+import java.util.Random;
+
 import com.amitinside.core.api.DataPayload;
 
 public final class MockDataProvider {
 
-	private static double doubleRandomInclusive(final double max, final double min) {
-		final double r = Math.random();
-		if (r < 0.5) {
-			return (((1 - Math.random()) * (max - min)) + min);
-		}
-		return ((Math.random() * (max - min)) + min);
+	private static double doubleRandomInclusive(final int start, final int end) {
+		final double random = new Random().nextDouble();
+		final double result = start + (random * (end - start));
+		return result;
 	}
 
 	public static DataPayload mock() {
 		final double longitude = Math.random() * Math.PI * 2;
 		final double latitude = Math.acos((Math.random() * 2) - 1);
 
-		return new DataPayload.Builder().co(doubleRandomInclusive(0.0d, 2.0d)).no2(doubleRandomInclusive(1.0d, 4.0d))
-				.o3(doubleRandomInclusive(1.0d, 3.0d)).so2(doubleRandomInclusive(3.0d, 6.0d)).latitude(latitude)
+		return new DataPayload.Builder().co(doubleRandomInclusive(0, 3)).no2(doubleRandomInclusive(1, 4))
+				.o3(doubleRandomInclusive(2, 5)).so2(doubleRandomInclusive(3, 6)).latitude(latitude)
 				.longitude(longitude).build();
 	}
 
