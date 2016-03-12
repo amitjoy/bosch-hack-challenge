4c10c1894af2fd4584381c9ccb644d8aae2055ca
Amit Kumar Mondal <admin@amitinside.com>
2016-03-12 11:43:20 +0100

    Added new Client Directory

--- com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
+++ com.amitinside.mock.data.provider/src/main/java/com/amitinside/mock/data/provider/MockDataProvider.java
@@ -6,7 +6,7 @@ import com.amitinside.core.api.DataPayload;
 
 public final class MockDataProvider {
 
-	private static double doubleRandomInclusive(final int start, final int end) {
+	private static double doubleRandomInclusive(final double start, final double end) {
 		final double random = new Random().nextDouble();
 		final double result = start + (random * (end - start));
 		return result;
@@ -15,7 +15,7 @@ public final class MockDataProvider {
 	public static DataPayload mock() {
 		return new DataPayload.Builder().co(doubleRandomInclusive(0, 3)).no2(doubleRandomInclusive(1, 4))
 				.o3(doubleRandomInclusive(2, 5)).so2(doubleRandomInclusive(3, 6))
-				.latitude(doubleRandomInclusive(52, 53)).longitude(doubleRandomInclusive(13, 14)).build();
+				.latitude(doubleRandomInclusive(52.4, 52.6)).longitude(doubleRandomInclusive(13.3, 13.5)).build();
 	}
 
 	private MockDataProvider() {
